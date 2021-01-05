6aafeee24f0c3c7896cd39ca4858b9fd3c6760d4
DiegoBrito <dibbrito@gmail.com>
2021-01-04 22:16:36 -0300

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'algamonei-ui';
+  lancamentos = [
+    {
+      tipo: 'DESPESA',
+      descricao: 'Compra de Pão',
+      dataVencimento: '30/06/2020',
+      dataPagamento: null,
+      valor: 4.55,
+      pessoa: 'Padaria do José',
+    },
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import {TabMenuModule} from 'primeng/tabmenu';
+import {InputTextModule} from 'primeng/inputtext';
+import {ButtonModule} from 'primeng/button';
+import {TableModule} from 'primeng/table';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TabMenuModule,
+    InputTextModule,
+    ButtonModule,
+    TableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
