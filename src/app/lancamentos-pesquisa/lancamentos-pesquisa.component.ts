302ff899ffebf7e2e7b497b614f179da7fdb72c6
DiegoBrito <dibbrito@gmail.com>
2021-01-07 21:55:12 -0300

    Create component for lancamentos

---
+++ src/app/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
@@ -0,0 +1,94 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lancamentos-pesquisa',
+  templateUrl: './lancamentos-pesquisa.component.html',
+  styleUrls: ['./lancamentos-pesquisa.component.css']
+})
+export class LancamentosPesquisaComponent {
+
+  title = 'algamonei-ui';
+  lancamentos = [
+    {
+      tipo: 'DESPESA',
+      descricao: 'Compra de Pão',
+      dataVencimento: '30/06/2020',
+      dataPagamento: null,
+      valor: 4.55,
+      pessoa: 'Padaria do José',
+    },
+    {
+      tipo: 'RECEITA',
+      descricao: 'Compra de Farinha',
+      dataVencimento: '30/06/2020',
+      dataPagamento: null,
+      valor: 4.55,
+      pessoa: 'Padaria do José',
+    },
+    {
+      tipo: 'RECEITA',
+      descricao: 'Compra de Leite',
+      dataVencimento: '30/06/2020',
+      dataPagamento: null,
+      valor: 8.99,
+      pessoa: 'Padaria do José',
+    },
+    {
+      tipo: 'DESPESA',
+      descricao: 'Compra de Pão',
+      dataVencimento: '30/06/2020',
+      dataPagamento: null,
+      valor: 4.55,
+      pessoa: 'Padaria do José',
+    },
+    {
+      tipo: 'DESPESA',
+      descricao: 'Compra de Pão',
+      dataVencimento: '30/06/2020',
+      dataPagamento: null,
+      valor: 4.55,
+      pessoa: 'Padaria do José',
+    },
+    {
+      tipo: 'DESPESA',
+      descricao: 'Compra de Pão',
+      dataVencimento: '30/06/2020',
+      dataPagamento: null,
+      valor: 4.55,
+      pessoa: 'Padaria do José',
+    },
+    {
+      tipo: 'DESPESA',
+      descricao: 'Compra de Pão',
+      dataVencimento: '30/06/2020',
+      dataPagamento: null,
+      valor: 4.55,
+      pessoa: 'Padaria do José',
+    },
+    {
+      tipo: 'DESPESA',
+      descricao: 'Compra de Pão',
+      dataVencimento: '30/06/2020',
+      dataPagamento: null,
+      valor: 4.55,
+      pessoa: 'Padaria do José',
+    },
+    {
+      tipo: 'DESPESA',
+      descricao: 'Compra de Pão',
+      dataVencimento: '30/06/2020',
+      dataPagamento: null,
+      valor: 4.55,
+      pessoa: 'Padaria do José',
+    },
+    {
+      tipo: 'DESPESA',
+      descricao: 'Compra de Pão',
+      dataVencimento: '30/06/2020',
+      dataPagamento: null,
+      valor: 4.55,
+      pessoa: 'Padaria do José',
+    },
+  ];
+
+}
