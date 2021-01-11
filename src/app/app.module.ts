9dd2f81d88018c3871da0f332329c04c5d9f4be1
DiegoBrito <dibbrito@gmail.com>
2021-01-11 10:16:57 -0300

    Add date picker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { PessoasPesquisaComponent } from './pessoas-pesquisa/pessoas-pesquisa.co
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LancamentoCadastroComponent } from './lancamento-cadastro/lancamento-cadastro.component';
 import { InputTextareaModule } from "primeng/inputtextarea";
+import {CalendarModule} from 'primeng/calendar';
 @NgModule({
   declarations: [
     AppComponent,
@@ -31,7 +32,8 @@ import { InputTextareaModule } from "primeng/inputtextarea";
     ButtonModule,
     TableModule,
     TooltipModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    CalendarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
